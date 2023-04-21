59d097308a2a0485b2cfa2158fc330517194b17e
Shiisoo <defer1616@gmail.com>
2023-04-21 10:35:57 +0200

    Ajoutez des fichiers projet.

---
+++ JeuDeCartes/JeuDeCartes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+/*
+CREER UN JEU DE 52 CARTES
+
+Votre programme propose un tirage aléatoire de 2 x 5 cartes.
+
+Joueur 1 : Carte 1 + carte 2 + carte3 + carte 4 + carte 5
+Joueur 2 : Carte 1 + carte 2 + carte3 + carte 4 + carte 5
+
+Vous vérifiez qui gagne le point carte contre carte.
+
+Le joueur qui cumule le plus de point sur le tirage a gagné.
+*/
+
+int main()
+{
+    cout << "Hello World!\n";
+}
